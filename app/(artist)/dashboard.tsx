a2c46a93269002a4540b84c8230127c3cab6394e
Victor-Kip <victor.kipngetich@strathmore.edu>
2026-02-04 12:13:50 +0300

    complete styling artist dashboard cards

--- app/(artist)/dashboard.tsx
+++ app/(artist)/dashboard.tsx
@@ -17,7 +17,7 @@ const Dashboard = () => {
                     <View className='border-2 border-dashed border-blue-300 rounded-2xl items-center justify-center py-10 mb-6 bg-gray-100'>
                         <Feather name="upload" size={48} color="gray" className='mb-3' />
                         <Text className='text-gray-400 text-center px-8 py-3 mb-4'>Drag and drop your audio files here</Text>
-                        <TouchableOpacity className='bg-blue-100 px-8 py-3 rounded-xl shadow-sm'>
+                        <TouchableOpacity className='bg-blue-100 px-8 py-3 rounded-xl'>
                             <Text className='font-bold text-gray-700'>Upload Audio</Text>
                         </TouchableOpacity>
                     </View>
@@ -42,7 +42,7 @@ const Dashboard = () => {
                             </TouchableOpacity>
                         </View>
                     </View>
-                    <TouchableOpacity className='bg-blue-300 px-6 py-3 rounded-xl items-center shadow-lg mb-10'>
+                    <TouchableOpacity className='bg-blue-300 px-6 py-3 rounded-xl items-center  mb-10'>
                         <Text className='text-white font-bold text-lg'>Publish Track</Text>
                     </TouchableOpacity>
                 
@@ -52,11 +52,11 @@ const Dashboard = () => {
                 return (
                     <View className="flex-1 ">
                          <Text className="text-white text-2xl font-bold">Stats </Text>
-                         <View className='bg-gray-100 rounded-2xl p-5 mb-4 shaodow-sm '>
+                         <View className='bg-gray-100 rounded-2xl p-5 mb-4  '>
                             <View className='flex-row justify-between items-center mb-4'>
                                 <View className='flex-row items-center'>
-                                    <View className='bg-blue-300 p-3 rounded-full mr-3'>
-                                        <Feather name="play" size={24} color="white" />
+                                    <View className='bg-indigo-300 p-3 rounded-full mr-3'>
+                                        <Feather name="play" size={24} color="white" className='ml-1'/>
                                     </View>
                                     <Text className='text-indigo-900 font-bold text-lg'>Total Plays</Text>
                                 </View>
@@ -65,9 +65,60 @@ const Dashboard = () => {
                             <View className='flex-row justify-between items-end mt-2'>
                                 <View>
                                     <Text className='text-indigo-900 font-bold text-lg'>Recent Plays</Text>
+                                    <Text className='text-indigo-900 text-sm '>(Last 7 days)</Text>
+                                </View>
+                                    <View className='flex-row items-end justify-between gap-1 h-16'>
+                                        {[10, 15, 12, 20, 25, 30, 28, 35].map((h,i) =>( 
+                                        <View key={i} style={{height:h,width:12}} className='bg-indigo-900 rounded-t-sm opacity-50'></View>
+                                         ))}
+                                    </View>
+                            </View>
+                            <Text className=' text-right text-indigo-400 text-sm mt-4'>30 listeners</Text>
+                         </View>    
+                         <View className='bg-gray-100 rounded-2xl p-5 mb-6  '>
+                            <View className='flex-row justify-between items-center mb-4'>
+                                <View className='flex-row items-center'>
+                                    <View className='bg-indigo-300 p-3 rounded-full mr-3'>
+                                        <MaterialCommunityIcons name="comment-outline" size={24} color="white" className='ml-1'/>
+                                    </View>
+                                    <Text className='text-indigo-900 font-bold text-lg'>Comments received</Text>
+                                </View>
+                                <Text className='text-indigo-900 font-bold text-2xl'>0</Text>
+                            </View>
+                            <View className="flex-row items-end gap-1 h-12 w-full">
+                                <View className="flex-1 flex-row items-end justify-between px-2 h-full">
+                                    {[ 20, 25, 30, 28, 35, 32, 40].map((h, i) => (
+                                        <View key={i} style={{ height: h, width: 12 }} className="bg-indigo-900 rounded-t-sm opacity-50" />
+                                         ))}
+                                        </View>
+                                </View>
+                        </View>    
+                        <Text className="text-white text-xl font-bold mb-4">Followers</Text>    
+                        <View className='bg-gray-100 rounded-2xl p-5 mb-10 '>
+                            <View className='flex-row justify-between items-start'>
+                                <View className='flex-row items-center'>
+                                    <View className='bg-indigo-300 p-3 rounded-full mr-3'>
+                                        <Feather name="users" size={24} color="white" className='ml-1'/>
+                                    </View>
+                                    <Text className='text-indigo-900 font-bold text-lg'>Total Followers</Text>
+                                </View>
+                                <View className='items-end'>
+                                    <Text className='text-indigo-900 font-bold text-2xl'>0</Text>
                                 </View>
                             </View>
-                         </View>                         
+                            <View className=' flex-row justify-between mt-4 items-end'>
+                                <View>
+                                <Text className='text-indigo-900 font-bold text-lg'>Recent Followers</Text>
+                                <Text className='text-indigo-900 text-sm '>(Last 7 days)</Text>
+                                </View>
+                                <View className='flex-row items-end justify-between gap-1 h-16'>
+                                    {[5,7,11,19,20,23,33].map((h,i) =>( 
+                                    <View key={i} style={{height:h,width:14}} className='bg-indigo-900 rounded-t-sm opacity-50'></View>
+                                    ))}
+                                </View>
+                            </View>
+                            <Text className=' text-right text-indigo-400 text-sm mt-4'>0 new followers</Text>
+                        </View>           
                     </View>
 
                 );
@@ -103,7 +154,7 @@ const Dashboard = () => {
                             </View>
                             <Text className='text-indigo-900 font-bold text-2xl '>$0.00</Text>
                         </View>     
-                        <TouchableOpacity className=' flex-row bg-blue-300 py-4 rounded-xl justify-center items-center shadow-lg mt-2 mb-8'>
+                        <TouchableOpacity className=' flex-row bg-blue-300 py-4 rounded-xl justify-center items-center mt-2 mb-8'>
                             <Text className='text-white font-bold text-lg mr-2'>Withdraw Funds</Text>
                             <Feather name="arrow-right" size={20} color="white" className='mt-1'/>
                         </TouchableOpacity>
@@ -156,7 +207,7 @@ const Dashboard = () => {
                         </TouchableOpacity>
                     </View>
                     {showContent()}
-                    <TouchableOpacity className='bg-red-500 px-6 py-3 rounded-xl items-center shadow-lg mb-10 mt-4'
+                    <TouchableOpacity className='bg-red-500 px-6 py-3 rounded-xl items-center mb-10 mt-4'
                     onPress={signOut}>
                         <Text className='text-white font-bold text-lg'>Logout</Text>
                     </TouchableOpacity>
